4a6ce357614840aeae3e963857c57af6466eb4e5
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2019-08-04 20:53:23 +0200

    Improve commandline tool

--- bin/restia.lua
+++ bin/restia.lua
@@ -7,5 +7,23 @@
 -- @script restia
 
 local commands = require 'restia.commands'
+local c        = require 'restia.colors'
 
-commands[...](select(2, ...))
+local help = [[
+Restia Commandline Utility
+--------------------------
+
+Available commands:
+]]..table.concat(commands, '\n')
+
+local command = commands[...]
+
+if command then
+	command(select(2, ...))
+else
+	if (...) then
+		print('Unknown command: '..c.red(...))
+	else
+		print(help)
+	end
+end
--- restia/commands.lua
+++ restia/commands.lua
@@ -1,35 +1,42 @@
 local utils = require 'restia.utils'
 
-return {
-  new = function(name)
-    name = name or 'application'
-    utils.build_dir(nil, {
-      [name] = {
-        ['.gitignore'] = table.concat({
-          'lua_modules/*',
-          '.luarocks',
-          '.secret/*',
-        }, "\n");
-        ['.secret'] = {};
-        ['locations.conf'] = table.concat({
-          'location = / {\n\tcontent_by_lua_file "controllers/front.lua";\n}';
-          'location /static {\n\tdeny all;\n}';
-          'location /favicon.png {\n\talias static/favicon.png;\n}';
-          'location ~ ^/(styles|javascript|images)/(.*) {\n\talias static/$1/$2;\n}';
-        }, '\n\n');
-        controllers = {
-          ['front.lua'] = 'local restia = require "restia"\n\nrestia.template "views/front" ()';
-        };
-        views = {
-          ['front.moonhtml'] = 'h1 "Hello World!"';
-        };
-        models = {};
-        lib = {};
+local commands = {}
 
-        -- Create local rock tree
-        ['.luarocks'] = {};
-        lua_modules = { lib = {} };
-      };
-    })
-  end
-}
+function commands.new(name)
+	name = name or 'application'
+	utils.build_dir(nil, {
+		[name] = {
+			['.gitignore'] = table.concat({
+				'lua_modules/*',
+				'.luarocks',
+				'.secret/*',
+			}, "\n");
+			['.secret'] = {};
+			['locations.conf'] = table.concat({
+				'location = / {\n\tcontent_by_lua_file "controllers/front.lua";\n}';
+				'location /static {\n\tdeny all;\n}';
+				'location /favicon.png {\n\talias static/favicon.png;\n}';
+				'location ~ ^/(styles|javascript|images)/(.*) {\n\talias static/$1/$2;\n}';
+			}, '\n\n');
+			controllers = {
+				['front.lua'] = 'local restia = require "restia"\n\nrestia.template "views/front" ()';
+			};
+			views = {
+				['front.moonhtml'] = 'h1 "Hello World!"';
+			};
+			models = {};
+			lib = {};
+
+			-- Create local rock tree
+			['.luarocks'] = {};
+			lua_modules = { lib = {} };
+		};
+	})
+end
+
+local idx = 1
+for name in pairs(commands) do
+	commands[idx] = name
+	idx = idx + 1
+end
+return commands
