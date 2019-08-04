5a1e3867662ebea11d3dee401757d928054291d4
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2019-08-04 20:50:23 +0200

    Add touch and delete to utils.build_dir
    touch: create a file if it does not exist
    delete: deletes a file if it exists

--- restia/utils.lua
+++ restia/utils.lua
@@ -1,11 +1,25 @@
 --- Utility functions for Restia
 
+local utils = {}
+
 local lfs = require 'lfs'
 local colors = require 'restia.colors'
 
+function utils.delete(path)
+	path = path:gsub('/+$', '')
+	local mode = lfs.attributes(path, 'mode')
+	if mode=='directory' then
+		for entry in lfs.dir(path) do
+			utils.delete(path..'/'..entry)
+		end
+	end
+	os.remove(path)
+end
+
 --- Builds a directory structure recursively from a table template.
 -- @tparam string prefix A prefix to the path, aka. where to initialize the directory structure.
--- @tparam table tab A table representing the directory structure. Table entries are subdirectories, strings are files, everything else is an error.
+-- @tparam table tab A table representing the directory structure.
+-- Table entries are subdirectories, strings are files, false means delete, true means touch file, everything else is an error.
 -- @usage
 -- 	build_dir {
 -- 		sub_dir = {
@@ -13,47 +27,69 @@ local colors = require 'restia.colors'
 -- 		}
 -- 		file = 'Hello World!';
 -- 	}
--- @todo add `true` option for empty file
--- @todo add `false` option to delete existing files/directories
-local function build_dir(prefix, tab)
-  for path, value in pairs(tab) do
-    if prefix then
-      path = prefix.."/"..tostring(path)
-    end
+-- @todo add `true` option for empty file -- @todo add `false` option to delete existing files/directories
+function utils.build_dir(prefix, tab)
+	if not tab then
+		tab = prefix
+		prefix = '.'
+	end
+	if not type(tab) == 'table' then
+		error("Invalid argument; expected table, got "..type(tab), 1)
+	end
 
-    if type(value) == "table" then
-      lfs.mkdir(path)
-      print (
-        "Directory  "
-        ..colors.blue(path)
-      )
-      build_dir(path, value)
+	for path, value in pairs(tab) do
+		if prefix then
+			path = prefix.."/"..tostring(path)
+		end
 
-    elseif type(value) == "string" then
-      print(
-        "File       "
-        ..colors.magenta(path)
-        .." with "
-        ..#value
-        .." bytes"
-      )
-      local file = io.open(path,'w')
-      file:write(value)
-      file:close()
+		if type(value) == "table" then
+			lfs.mkdir(path)
+			print (
+				"Directory  "
+				..colors.blue(path)
+			)
+			utils.build_dir(path, value)
 
-    else
-      print(
-        "Unknown type at     "
-        ..colors.red(path)
-        .." ("
-        ..colors.red(type(value))
-        ..")"
-      )
+		elseif type(value) == "string" then
+			print(
+				"File       "
+				..colors.magenta(path)
+				.." with "
+				..#value
+				.." bytes"
+			)
+			local file = io.open(path,'w')
+			file:write(value)
+			file:close()
 
-    end
-  end
+		elseif value==false then
+			print(
+				"Deleting   "..colors.red(path)
+			)
+			utils.delete(path)
+
+		elseif value==true then
+			print(
+				"Touching   "..colors.yellow(path)
+			)
+			local file = io.open(path)
+			if not file then
+				file = io.open(path, 'w')
+				file:write('')
+			end
+			file:close()
+
+		else
+			print(
+				"Unknown type at     "
+				..colors.red(path)
+				.." ("
+				..colors.red(type(value))
+				..")"
+			)
+
+		end
+	end
 end
 
-return {
-  build_dir = build_dir
-}
+return utils
