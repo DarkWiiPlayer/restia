55f07eb1c318608da6b3b394a6ae3526c80b4b46
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2019-08-23 16:50:35 +0200

    Add view load test to template project

--- restia/commands.lua
+++ restia/commands.lua
@@ -2,6 +2,21 @@ local utils = require 'restia.utils'
 
 local commands = {}
 
+local nginx = [[nginx -p . -c openresty.conf -g 'daemon off;' ]]
+
+local test_views_load = [===========[
+restia = require 'restia'
+utils = require 'restia.utils'
+
+describe 'View', ->
+	before_each ->
+		_G.ngx = {print: ->}
+	for file in utils.files 'views'
+		describe file, ->
+			it 'should load', ->
+				restia.template file\gsub('%..-$', '')
+]===========]
+
 function commands.new(name)
 	name = name or 'application'
 	utils.build_dir(nil, {
@@ -26,6 +41,13 @@ function commands.new(name)
 			};
 			models = {};
 			lib = {};
+			spec = {
+				views = {
+					['load_spec.moon'] = test_views_load;
+				};
+			};
+
+			logs = {};
 
 			-- Create local rock tree
 			['.luarocks'] = {};
