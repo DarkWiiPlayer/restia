4dcb5c116a920b6402c0286c03b84203adc62781
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2019-10-02 22:03:03 +0200

    Improved commands system
    - Move actual command definitions into executable
    - Add description for every command
    - Add help command and improve help output

--- bin/restia.lua
+++ bin/restia.lua
@@ -7,14 +7,146 @@
 -- @script restia
 
 local commands = require 'restia.commands'
-local c        = require 'restia.colors'
+local utils = require 'restia.utils'
+local c = require 'restia.colors'
 
 local help = [[
 Restia Commandline Utility
 --------------------------
 
 Available commands:
-]]..table.concat(commands.listing, '\n')
+]]
+
+local nginx = [[nginx -p . -c openresty.conf -g 'daemon off;' ]]
+
+local test_views_load = [===========[
+restia = require 'restia'
+utils = require 'restia.utils'
+
+describe 'View', ->
+	before_each ->
+		_G.ngx = {print: ->}
+	for file in utils.files 'views'
+		describe file, ->
+			it 'should load', ->
+				restia.template file\gsub('%..-$', '')
+]===========]
+
+local openresty_conf =
+[===========[
+error_log logs/error.log;
+error_log logs/error.log  notice;
+error_log logs/error.log  info;
+pid openresty.pid;
+
+events {
+	worker_connections	1024;
+}
+
+http {
+	lua_code_cache off; # Change this for production
+
+	lua_package_path 'lua_modules/share/lua/5.1/?.lua;lua_modules/share/lua/5.1/?/init.lua;;';
+	lua_package_cpath 'lua_modules/lib/lua/5.1/?.so;lua_modules/lib/lua/5.1/?/init.so;;';
+
+	log_format  main '$remote_addr - $remote_user [$time_local] "$request" '
+	                 '$status $body_bytes_sent "$http_referer" '
+	                 '"$http_user_agent" "$http_x_forwarded_for"';
+
+	access_log logs/access.log main;
+	keepalive_timeout 65;
+
+	default_type text/html;
+	charset utf-8;
+
+	init_by_lua_block {
+		require 'restia'
+	}
+
+	server {
+		listen 8080;
+
+		include routes/*;
+	}
+}
+]===========]
+
+commands:add('new <directory>', [[
+	Creates a new application in the selected directory.
+	The default <directory> is 'application'.
+]], function(name)
+	name = name or 'application'
+	utils.build_dir(nil, {
+		[name] = {
+			['.gitignore'] = table.concat({
+				'lua_modules/*',
+				'.luarocks',
+				'.secret/*',
+				'logs/*',
+				'*.pid',
+			}, "\n");
+			['.secret'] = {};
+			['openresty.conf'] = openresty_conf;
+			routes = {
+				root = 'location = / {\n\tcontent_by_lua_file "controllers/front.lua";\n}';
+				static = 'location /static {\n\tdeny all;\n}'
+					..'\nlocation /favicon.png {\n\talias static/favicon.png;\n}'
+					..'\nlocation ~ ^/(styles|javascript|images)/(.*) {\n\talias static/$1/$2;\n}';
+			};
+			controllers = {
+				['front.lua'] = 'local restia = require "restia"\n\nrestia.templates["views/front"]()';
+			};
+			views = {
+				['front.moonhtml'] = 'h1 "Hello World!"';
+			};
+			models = {};
+			lib = {};
+			spec = {
+				views = {
+					['load_spec.moon'] = test_views_load;
+				};
+			};
+
+			logs = {};
+
+			-- Create local rock tree
+			['.luarocks'] = {};
+			lua_modules = {};
+		};
+	})
+end)
+
+commands:add('test <configuration>', [[
+	Runs several tests:
+	- 'nginx -t' to check the nginx configuration
+	- 'luacheck' for static analisys of the projects Lua files
+	- 'busted'   to run the projects tests
+	<configuration> defaults to openresty.conf
+]], function(configuration)
+	configuration = configuration or 'openresty.conf'
+	os.execute(nginx:gsub('openresty.conf', configuration)..'-t')
+	print()
+	os.execute('luacheck -q .')
+	os.execute('busted')
+end)
+
+commands:add('run <configuration>', [[
+	Runs an application in the current directory.
+	Default for <configuration> is 'openresty.conf'.
+]], function(config)
+	config = config or 'openresty.conf'
+	os.execute(nginx:gsub('openresty.conf', config))
+end)
+
+commands:add('help', [[
+	Prints this help and exits.
+]], function()
+	print(help)
+	for idx, command in ipairs(commands) do
+		print((c('%{green}'..command.name):gsub('<.->', c.blue)))
+		print((command.description:gsub('<.->', c.blue)))
+	end
+end)
 
 local command = commands[...]
 
@@ -24,6 +156,6 @@ else
 	if (...) then
 		print('Unknown command: '..c.red(...))
 	else
-		print(help)
+		commands.help()
 	end
 end
