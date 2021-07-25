c66172d9d81d2a2bedd43f0a395d61527ef7d835
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2021-07-25 13:29:31 +0200

    Document controller class with usage example

--- restia/controller.lua
+++ restia/controller.lua
@@ -1,8 +1,21 @@
 --- A base class for stateful controllers à la Rails
--- @module restia.controller
+-- @classmod restia.controller
+-- @usage
+-- local users = controller:new()
+-- function users:get_user(id)
+-- 	-- get a user from somewhere
+-- end
+-- function users:show(req)
+-- 	get_user(req.params.id)
+-- 	-- do stuff here
+-- end
+-- return users
 
 local request = require 'restia.request'
 local protomixin = require 'restia.protomixin'
+
+--- Creates a new (empty) controller
+-- @function new
 
 local controller = protomixin.new(request, {new=protomixin.new})
 
