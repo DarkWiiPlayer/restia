c8863061632fb7115182f06c7bddcea7b3ff2110
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2022-03-24 12:31:11 +0100

    Add optional extension parameter to post loader

--- restia/config/post.lua
+++ restia/config/post.lua
@@ -11,10 +11,12 @@ local readfile = require 'restia.config.readfile'
 -- May return non-table values for invalid YAML,
 -- as CYAML supports other types than Object at
 -- the top-level of the YAML file.
+-- @tparam string name Name of the file to load
+-- @tparam[opt=".post"] string extension File extension to append to the file
 -- @treturn table YAML-Data
 -- @function load
-return function(name)
-	local file = io.open(name .. '.post')
+return function(name, extension)
+	local file = io.open(name .. (extension or '.post'))
 	if file then
 		local head, body = restia.utils.frontmatter(file:read('a'))
 		return {
